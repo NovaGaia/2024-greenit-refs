e0fcd192e3f2ff8a0c056d95bdc86bfa358b82ae
Renaud Heluin <renaud.heluin@novagaia.fr>
2024-01-22 21:25:31 +0000

    Add Card and CardView components

---
+++ src/components/Card.ts
@@ -0,0 +1,8 @@
+Card.PERSONAS = "personas";
+Card.FICHES = "fiches";
+Card.LEXIQUE = "lexique";
+Card.VERTICAL_LAYOUT = "vertical";
+Card.HORIZONTAL_LAYOUT = "horizontal";
+
+function Card() {}
+export default Card;
