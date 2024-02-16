e8d706488c246d1c9910eb8a6e8ef57d3f32d8e0
Renaud Heluin <renaud.heluin@novagaia.fr>
2024-02-16 15:13:44 +0000

    Add API routes for fetching fiche by ID and fetching all fiches

---
+++ src/pages/api/[lang]/getFicheById/[id].ts
@@ -0,0 +1,49 @@
+import { getCollection, type CollectionEntry } from "astro:content";
+import type { EntryType } from "perf_hooks";
+import { getRefConfig } from "referentiel-config";
+
+export async function GET({ params, request }) {
+  const id = params.id;
+  const lang = params.lang;
+  const entries: CollectionEntry<"fiches">[] = await getCollection("fiches");
+  const filteredEntries: CollectionEntry<"fiches">[] = entries
+    .filter((entry) => entry.data.language === lang)
+    .filter((entry) => entry.data.published)
+    .filter((entry) => entry.data.refID === params.id);
+  if (entries && filteredEntries.length > 1) {
+    throw new Error("Duplicate ID on `Fiches`.");
+  }
+  const entry = filteredEntries[0];
+  const [_lang, ...slug] = entry.slug.split("/");
+  let o = {
+    id: entry.data.refID,
+    lang: lang,
+    title: entry.data.title,
+    lifecycle: entry.data.lifecycle,
+    slug: `/${import.meta.env.PUBLIC_BASE !== "" ? import.meta.env.PUBLIC_BASE + "/" : ""}fr/${entry.collection}/${slug}`,
+  };
+  if (getRefConfig().featuresEnabled.scope) {
+    o["scope"] = entry.data.scope;
+  }
+  return new Response(JSON.stringify(o));
+}
+
+export async function getStaticPaths() {
+  const entries = await getCollection("fiches");
+  const paths = entries
+    .filter((entry) =>
+      import.meta.env.MODE === "development" ? "true" : entry.data.published,
+    )
+    .map((entry) => {
+      const [lang, ...slug] = entry.slug.split("/");
+      return {
+        params: {
+          lang,
+          id: entry.data.refID,
+        },
+        props: { entry, entries },
+      };
+    });
+
+  return paths;
+}
